75c559376556d0c9909e543bc20e0ec8dac2a290
mt-richard <Richard@festimoltd.com>
2023-05-09 21:25:52 +0200

    working in js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const {users} = require('./models/index.js');
 const { json } = require("sequelize");
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(cookieParser());
 app.use(express.json());
@@ -42,14 +46,16 @@ app.get('/', function(req, res) {
 
 //Login 
 app.post('/login', async (req, res) =>{
-    const { email, password } = req.body
+    // const { email, password } = req.body
+    const email = req.body.email;
+    const password = req.body.password;
     const userlog = await users.findOne({ where: {email, password}})
   
     // res.json(userlog)
     if (userlog) {
         const token = jwt.sign({ user: { email } }, secretKey, {expiresIn: '5m'});
         res.cookie('token', token, { httpOnly: true, maxAge: 3600000 });
-        res.redirect(301, '/dashboard');
+        res.json({message: 'ok'});
        
         // res.json(token)
     } else {
@@ -59,33 +65,33 @@ app.post('/login', async (req, res) =>{
 
 //signup
 app.post('/signup', async (req, res) => {
-    const { name, email, password } = req.body;
+    // const { name, email, password } = req.body;
+        
     try {
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
       const insert = await users.create({ name, email, password });
       if (insert) {
         res.json({ message: 'User created successfully!' });
       }
     } catch (error) {
-      console.log(error);
+    //   console.log(error);
       res.status(500).json({ message: 'Error creating user' });
     }
   });
   
 
-
 //logout 
 app.get('/logout', (req, res) => {
-   
     res.cookie('token', '', { expires: new Date(0) });
-    const message ='Logout successfull';
-    res.redirect('/',401,{message});
+    res.redirect('/');
   });
   
 
-
 //dashboard
 app.get('/dashboard', authenticate, (req, res)=>{
-    res.status(200).sendFile(__dirname + '/views/dashboard.html')
+    res.sendFile(__dirname + '/views/dashboard.html')
 })
 app.get('/users', authenticate, async(req, res) =>{
     const allusers = await users.findAll()
